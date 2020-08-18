ae0ca1588743e138ac1f185da18c194dc6bcd47b
andrew-bingham-dev <andrew.bingham.dev@gmail.com>
2020-08-18 17:30:09 +0100

    basic song list created

--- src/components/FeaturedSong.js
+++ src/components/FeaturedSong.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const FeaturedSong = ({ songTitle }) => {
+const FeaturedSong = ({ songTitle, releaseDate, theme, image }) => {
   return (
-    <div className='featured-song'>
-      <h2>{songTitle}</h2>
+    <div className={`featured-song ${theme}`}>
+      <div className='song-container'>
+        <div className='song-image'>
+          <img src={image} />
+        </div>
+        <div className='song-data'>
+          <h3 className='heading'>Title</h3>
+          <h3 className='data'>{songTitle}</h3>
+          <br />
+          <h3 className='heading'>Release date</h3>
+          <h3 className='data'>{releaseDate}</h3>
+          <br />
+          <h3 className='heading'>Play this song on</h3>
+          <button>Spotify</button>
+          <button>Apple Music</button>
+          <button>YouTube</button>
+          <br />
+          <br />
+          <h3 className='heading'>Buy this song on</h3>
+          <button>Amazon</button>
+          <button>Google Play Music</button>
+          <button>CD Baby</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/FeaturedSongList.js
+++ src/components/FeaturedSongList.js
@@ -4,9 +4,24 @@ import FeaturedSong from './FeaturedSong';
 const FeaturedSongList = () => {
   return (
     <div className='featured-song-list'>
-      <FeaturedSong songTitle='Set the World on Fire' />
-      <FeaturedSong songTitle='Haunted' />
-      <FeaturedSong songTitle='Call My Name' />
+      <FeaturedSong
+        songTitle='Set the World on Fire'
+        releaseDate='1st July 2019'
+        theme='black'
+        image={require('../images/set-the-world-on-fire-cover.png')}
+      />
+      <FeaturedSong
+        songTitle='Haunted'
+        releaseDate='1st July 2019'
+        theme='white'
+        image={require('../images/haunted-cover.png')}
+      />
+      <FeaturedSong
+        songTitle='Call My Name'
+        releaseDate='1st July 2019'
+        theme='black'
+        image={require('../images/call-my-name-cover.jpg')}
+      />
     </div>
   );
 };
